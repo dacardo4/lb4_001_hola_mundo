867293555996ec3803d2418759ff32265f2c0c04
prueba <42043332+dacardo4@users.noreply.github.com>
2020-06-15 22:18:05 -0500

    implements login/register with JWT

--- src/sequence.ts
+++ src/sequence.ts
@@ -1,3 +1,11 @@
+// ---------- ADD IMPORTS -------------
+import {
+  AuthenticateFn,
+  AuthenticationBindings,
+  AUTHENTICATION_STRATEGY_NOT_FOUND,
+  USER_PROFILE_NOT_FOUND
+} from '@loopback/authentication';
+// ------------------------------------
 import {inject} from '@loopback/core';
 import {
   FindRoute,
@@ -8,7 +16,7 @@ import {
   RequestContext,
   RestBindings,
   Send,
-  SequenceHandler,
+  SequenceHandler
 } from '@loopback/rest';
 
 const SequenceActions = RestBindings.SequenceActions;
@@ -27,6 +35,8 @@ export class MySequence implements SequenceHandler {
     @inject(SequenceActions.INVOKE_METHOD) protected invoke: InvokeMethod,
     @inject(SequenceActions.SEND) public send: Send,
     @inject(SequenceActions.REJECT) public reject: Reject,
+    // ---- ADD THIS LINE ------
+    @inject(AuthenticationBindings.AUTH_ACTION) protected authenticateRequest: AuthenticateFn,
   ) {}
 
   async handle(context: RequestContext) {
@@ -35,10 +45,24 @@ export class MySequence implements SequenceHandler {
       const finished = await this.invokeMiddleware(context);
       if (finished) return;
       const route = this.findRoute(request);
+      // - enable jwt auth -
+      // call authentication action
+      // ---------- ADD THIS LINE -------------
+      await this.authenticateRequest(request);
       const args = await this.parseParams(request, route);
       const result = await this.invoke(route, args);
       this.send(response, result);
     } catch (err) {
+      // ---------- ADD THIS SNIPPET -------------
+      // if error is coming from the JWT authentication extension
+      // make the statusCode 401
+      if (
+        err.code === AUTHENTICATION_STRATEGY_NOT_FOUND ||
+        err.code === USER_PROFILE_NOT_FOUND
+      ) {
+        Object.assign(err, {statusCode: 401 /* Unauthorized */});
+      }
+      // ---------- END OF SNIPPET -------------
       this.reject(context, err);
     }
   }
